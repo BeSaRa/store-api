05cb481ae203ea71d72688205a64ad7954fa13b3
BeSaRa <besara@windowslive.com>
2021-12-19 08:48:04 +0300

    refactor: Make relation ON DELETE to CASCADE

--- migrations/sqls/20211216234803-orders-table-up.sql
+++ migrations/sqls/20211216234803-orders-table-up.sql
@@ -1,6 +1,6 @@
 /* Replace with your SQL commands */
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY,
-  user_id integer REFERENCES users(id),
+  user_id integer REFERENCES users(id) ON DELETE CASCADE,
   status boolean
 );
--- migrations/sqls/20211216235419-order-products-up.sql
+++ migrations/sqls/20211216235419-order-products-up.sql
@@ -1,7 +1,7 @@
 /* Replace with your SQL commands */
 CREATE TABLE order_products (
   id SERIAL PRIMARY KEY,
-  product_id bigint REFERENCES products(id),
-  order_id bigint REFERENCES orders(id),
+  product_id bigint REFERENCES products(id) ON DELETE CASCADE,
+  order_id bigint REFERENCES orders(id) ON DELETE CASCADE,
   quantity integer NOT NULL
 );
