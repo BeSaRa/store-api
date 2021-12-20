b2269c080fe298978d114b3c3fbc53d4f3f96630
BeSaRa <besara@windowslive.com>
2021-12-20 14:42:23 +0300

    feat: Active/Complete order endpoint for users

--- src/handlers/userRoutes.ts
+++ src/handlers/userRoutes.ts
@@ -1,9 +1,11 @@
 import { Application, Request, Response } from "express";
 import UserStore from "../models/userStore";
 import { User } from "../interfaces/user";
+import OrderStore from "../models/orderStore";
+import { OrderStatus } from "../enums/order-status";
 
 const userStore = new UserStore();
-
+const orderStore = new OrderStore();
 const index = async (_req: Request, res: Response) => {
   res.send(await userStore.index());
 };
@@ -34,10 +36,36 @@ const update = async (req: Request, res: Response) => {
   }
 };
 
+const userOrders = async (req: Request, res: Response) => {
+  res.send(await orderStore.getUserOrders(parseInt(req.params.id)));
+};
+
+const getOrderProducts = async (req: Request, res: Response) => {
+  res.send(await orderStore.getOrderProducts(parseInt(req.params.orderId)));
+};
+
+const activeUserOrders = async (req: Request, res: Response) => {
+  res.send(
+    await orderStore.getUserOrders(parseInt(req.params.id), OrderStatus.ACTIVE)
+  );
+};
+const completeUserOrders = async (req: Request, res: Response) => {
+  res.send(
+    await orderStore.getUserOrders(
+      parseInt(req.params.id),
+      OrderStatus.COMPLETE
+    )
+  );
+};
+
 export default function userRoutes(app: Application): void {
   app.get("/users", index);
   app.post("/users", create);
   app.get("/users/:id", show);
   app.put("/users/:id", update);
   app.delete("/users/:id", destroy);
+  app.get("/users/:id/orders/active", activeUserOrders);
+  app.get("/users/:id/orders/complete", completeUserOrders);
+  app.get("/users/:id/orders", userOrders);
+  app.get("/users/:id/orders/:orderId/products", getOrderProducts);
 }
